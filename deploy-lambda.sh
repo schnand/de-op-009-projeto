5bbfd6581e137afd5a00aae277622df985775fe0
schnand <schneider.and146@gmail.com>
2023-04-25 16:33:49 -0300

    inicial final

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,7 +1,7 @@
-#pip install --platform manylinux2014_x86_64 --target=lambda_function --implementation cp --python 3.9 --only-binary=:all: --upgrade psycopg2-binary
-#pip install --platform manylinux2014_x86_64 --target=lambda_function --implementation cp --python 3.9 --only-binary=:all: --upgrade pandas
+pip install --platform manylinux2014_x86_64 --target=lambda_function --implementation cp --python 3.9 --only-binary=:all: --upgrade psycopg2-binary
+pip install --platform manylinux2014_x86_64 --target=lambda_function --implementation cp --python 3.9 --only-binary=:all: --upgrade pandas
 
-#terraform init
+terraform init
 terraform apply -auto-approve
 
 BUCKET_NAME=$(terraform output -raw bucket_name)
