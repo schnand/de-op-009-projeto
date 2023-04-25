4c3216202b8db7a690d25350de998c472333865c
schnand <schneider.and146@gmail.com>
2023-04-25 16:35:19 -0300

    final

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -6,5 +6,5 @@ terraform apply -auto-approve
 
 BUCKET_NAME=$(terraform output -raw bucket_name)
 
-aws s3 cp train.csv s3://$BUCKET_NAME --profile default
+aws s3 cp train.csv s3://$BUCKET_NAME --profile default  
 
